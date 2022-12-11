52a7e72ab908577992e9bcd7a219d5d14d3a1ba9
Rey276 <65110492+Rey276@users.noreply.github.com>
2022-12-10 23:47:47 -0800

    Update HotButteredRumActivity.java

--- app/src/main/java/com/example/coffeeappv1/HotButteredRumActivity.java
+++ app/src/main/java/com/example/coffeeappv1/HotButteredRumActivity.java
@@ -13,4 +13,29 @@ public class HotButteredRumActivity extends AppCompatActivity {
         setContentView(R.layout.activity_hot_buttered_rum);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
+    
+    public void buttonPressKahluaCream(View view) {
+        Intent intent = new Intent(this, KahluaCreamActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressEspressoNuovo(View view) {
+        Intent intent = new Intent(this, EspressoNuovoActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressViennaDark(View view) {
+        Intent intent = new Intent(this, ViennaDarkActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressFrenchRoastDarkItalian(View view) {
+        Intent intent = new Intent(this, FrenchRoastDarkItalianActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressChocolateMacadamiaNut(View view) {
+        Intent intent = new Intent(this, ChocolateMacadamiaNutActivity.class);
+        startActivity(intent);
+    }
 }
