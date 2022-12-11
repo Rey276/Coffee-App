cd2c9ba3c5c21d42f004dd98890a664f4698da9b
Rey276 <65110492+Rey276@users.noreply.github.com>
2022-12-10 23:43:24 -0800

    Update HawaiianKonaFancyActivity.java

--- app/src/main/java/com/example/coffeeappv1/HawaiianKonaFancyActivity.java
+++ app/src/main/java/com/example/coffeeappv1/HawaiianKonaFancyActivity.java
@@ -13,4 +13,29 @@ public class HawaiianKonaFancyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_hawaiian_kona_fancy);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
+    
+    public void buttonPressOrganicPeru(View view) {
+        Intent intent = new Intent(this, OrganicPeruActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressEthiopiaMoka(View view) {
+        Intent intent = new Intent(this, EthiopiaMokaActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressHawaiianGoldenBlend(View view) {
+        Intent intent = new Intent(this, HawaiianGoldenBlendActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressColombiaFrench(View view) {
+        Intent intent = new Intent(this, ColombiaFrenchActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressIslandBlend(View view) {
+        Intent intent = new Intent(this, IslandBlendActivity.class);
+        startActivity(intent);
+    }
 }
