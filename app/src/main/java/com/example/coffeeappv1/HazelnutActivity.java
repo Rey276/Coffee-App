bd7176b1c80a11145d27ce606fe47f03d6e76a86
Rey276 <65110492+Rey276@users.noreply.github.com>
2022-12-10 23:45:37 -0800

    Update HazelnutActivity.java

--- app/src/main/java/com/example/coffeeappv1/HazelnutActivity.java
+++ app/src/main/java/com/example/coffeeappv1/HazelnutActivity.java
@@ -13,4 +13,29 @@ public class HazelnutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_hazelnut);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
     }
+
+    public void buttonPressDecaffeinatedHazelnut(View view) {
+        Intent intent = new Intent(this, DecaffeinatedHazelnutActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressCinnamonFrangelico(View view) {
+        Intent intent = new Intent(this, CinnamonFrangelicoActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressChocolateMacadamiaNut(View view) {
+        Intent intent = new Intent(this, ChocolateMacadamiaNutActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressEspressoNuovo(View view) {
+        Intent intent = new Intent(this, EspressoNuovoActivity.class);
+        startActivity(intent);
+    }
+
+    public void buttonPressDecaffeinatedVanillaNutCream(View view) {
+        Intent intent = new Intent(this, DecaffeinatedVanillaNutCreamActivity.class);
+        startActivity(intent);
+    }
 }
